1a555edb918def6a8e9945076cf2f52c5538918b
Dmitriy Kolesov <kolesov.fsd@gmail.com>
2023-04-28 13:09:12 +0500

    Описание хранилища для Корзины

--- src/stores/bascket.js
+++ src/stores/bascket.js
@@ -2,12 +2,32 @@ import { defineStore } from 'pinia'
 
 export const useBascketStore = defineStore('bascket',{
   state: () => ({
-
+    bascket: [],
+    quantity: 0,
+    amount: 0,
   }),
-  getters: {
-
-  },
   actions: {
-    
+    add(dishe){
+      this.quantity++;
+      this.amount+=dishe.price;
+      this.bascket.push(dishe);
+    },
+    remove(dishe, quantity){
+      this.quantity--;
+      this.amount-=dishe.price*quantity;
+      const index = this.bascket.indexOf(dishe);
+      this.bascket.splice(index, 1);
+    },
+    isContain(dishe){
+      return this.bascket.indexOf(dishe) === -1 ? false : true;
+    },
+    updateAmount(price){
+      this.amount+=price;
+    },
+    reset(){
+      this.bascket = [];
+      this.quantity = 0;
+      this.amount = 0;
+    }
   }
 })
